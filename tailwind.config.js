435984e5e24d382a08870d34b9ea86b75c03aed7
vijay dankhara <vijaydankhara111@gmail.com>
2024-07-06 23:37:34 +0530

    MyPortfolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'vijay': ['#72b626']
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
